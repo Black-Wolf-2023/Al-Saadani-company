352b4fdfbaf2534bab61464aa427e54dc29d0082
Abdalrhman Eldip <75347437+Abdalrhman8000@users.noreply.github.com>
2022-09-26 15:55:26 +0200

    Update some words

--- src/components/about/ViewComp.vue
+++ src/components/about/ViewComp.vue
@@ -3,17 +3,17 @@
     <div class="text">
       <h1>ABOUT US</h1>
       <p>
-        Originally founded by two brothers in the state of Maine, the Rentallo
-        company outlived one of them (a proud death in a combat while on an Iraq
-        war 2nd tour) and since then our farming vehicles rental venture moved
-        to Dallas, TX…
+        Originally founded by Engineer Ashraf Fahmy in the state of Albahara, the AL SAADANI 
+        company help to make world being better and push to make planets growing 
+         and since then our farming vehicles rental venture moved
+         to Damanhour 9 El-Gaish St
       </p>
       <p>
         The core values that our company always thrives to live by include
         integrity, hunger for constant gradual improvement and the affordability
         of our prices. Altogether, these 3 principles allowed us to become one
         of the most popular farming machinery rental outlet in the whole county
-        of Dallas-Fort Worth… More to that, we always keep track of the state of
+        of Albahara More to that, we always keep track of the state of
         our vehicles, making sure that our monthly repair checkups and the
         bi-yearly maintenance and replacements plans are keeping all of our
         farming vehicles intact and in good working condition! In our company’s
